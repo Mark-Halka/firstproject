546890b6e01521f74a5c628d011b627e0d11191c
Mark-Halka <markhalka2929@gmail.com>
2021-12-08 18:53:51 +0200

    for.1

--- lessons/lessons/Program.cs
+++ lessons/lessons/Program.cs
@@ -1,19 +1,8 @@
-﻿int count = -1;
-while (count < 4)
+﻿
+
+int a = int.Parse(Console.ReadLine());
+for (int i = int.Parse(Console.ReadLine()); i < a + 1; i++)
 {
-    count++;
-    Console.WriteLine(count);
+    Console.WriteLine(i);
 }
 
-int count1 = -1;
-do
-{
-    count1++;
-    Console.WriteLine(count1);
-} while (count1 < 4);
-int count2 = 10;
-while (count2 > 4)
-{
-    count2--;
-    Console.WriteLine(count2);
-}
