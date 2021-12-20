ceaa6df1e3969bdb17e5a05001deae049a90f17c
Mark-Halka <markhalka2929@gmail.com>
2021-12-20 13:42:03 +0200

    break and continue

--- lessons/lessons/Program.cs
+++ lessons/lessons/Program.cs
@@ -1,18 +1,23 @@
-﻿int a = 1000;
-for (; ; )
-{
-    a--;
-    a--;
-    a--;
-    a--;
-    a--;
-    a--;
-    a--;
-    Console.WriteLine(a);
-    System.Threading.Thread.Sleep(1);
-}
-
-for (int i = 5; i >= 0; i--)
+﻿
+//break це червона кнопка яка зупиняє цикл
+for (int i = 0; i <= 100; i++)
 {
     Console.WriteLine(i);
+    string a = Console.ReadLine();
+    if (a == "exit")
+    {
+        break;
+    }
 }
+
+//continue це червона кнопка яка пропускає якісь значння 
+for (int i = 0; i <= 100 ; i++)
+{
+    if (i % 2 == 0)
+    {
+        continue;
+    }
+    Console.WriteLine(i);
+}
+
+
