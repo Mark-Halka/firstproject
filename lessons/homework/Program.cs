4731ec27db7bc0c0c254f63247ce57140c856784
Mark-Halka <markhalka2929@gmail.com>
2022-01-22 16:20:44 +0200

    arrey.something part 1

--- lessons/homework/Program.cs
+++ lessons/homework/Program.cs
@@ -8,10 +8,54 @@ namespace homework
         static void Main(string[] args)
         {
 
+            //1
 
+            Console.Write("how many elemants will be in arrey : ");
 
+            int b = int.Parse(Console.ReadLine());
 
+            int[] n = new int[b];
 
+            for (int i = 0; i < n.Length; i++)
+            {
+                Console.Write($"Write element {i}:");
+                n[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.ReadLine();
+
+            Console.Clear();
+
+            //2
+
+            for (int i = 3; i >= 0; i--)
+            {
+                Console.WriteLine(n[i]);
+            }
+
+            Console.ReadLine();
+
+            Console.Clear();
+            //3
+
+            int NumberOfEvenNumbers = 0;
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (n[i] % 2 == 0)
+                {
+                    NumberOfEvenNumbers++;
+                }
+
+            }
+            Console.WriteLine(NumberOfEvenNumbers);
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+
+            Console.Clear();
+            
         }
     }
 }
--- lessons/lessons/Program.cs
+++ lessons/lessons/Program.cs
@@ -1,39 +1,40 @@
 ﻿
 using System;
 
-namespace homeworlessons
+namespace lessons
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            int[] a;
-            //зробили масив
-            a = new int[2];
-            //видилили місце на 5 елементів
-            a[1] = 6;
-            //положили 6 в другу квартиру
-            a[0] = 20;
-            //положили 20 в першу квартиру
-            int t = a[0];
-            //витянули 20 з першоє квартири і положили в зміну  t
-            Console.WriteLine(a[1]);
-            //вивели другу квартиру в консолі(занчення - 6)
-            Console.WriteLine(t);
-            //вивели t в консолі(занчення - 20)
-            Console.WriteLine(a[0]);
-            //вивели першу кваритиру в консолі(занчення - 20)
 
-            object[] b = new object[2];
-            b[0] = "Andriy";
-            b[1] = 15;
-            Console.WriteLine("hi my name is " + b[0] + ", and I`m " + b[1]);
-            
+            int[] a = { 1, 2, 3, 4, 5, 1,};
+
+            int b = a.Min();
+
+            int c = a.Max();
+
+            int d = a.Sum();
+
+            int e = a.Where(i => i % 2 == 0).Sum();
+
+            int f = a.Where(i => i % 2 != 0).Min();
+
+            int[] g = a.Distinct().ToArray();
+
+            int[] h = a.OrderBy(i => i).ToArray();
+
+            Console.WriteLine(c);
+
+            Console.WriteLine(b);
+
+            Console.WriteLine(d);
+
+            Console.WriteLine(e);
+
+            Console.WriteLine();
 
             Console.ReadLine();
-
-
-
 
         }
     }
