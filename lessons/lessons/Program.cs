e286e8f460a366c623169e9b91c85ab8514820f3
Mark-Halka <markhalka2929@gmail.com>
2022-01-31 18:40:32 +0200

    TWO-DIMENSIONAL ARROW

--- lessons/lessons/Program.cs
+++ lessons/lessons/Program.cs
@@ -7,34 +7,25 @@ namespace lessons
     {
         static void Main(string[] args)
         {
+            int[,] b =
+            {
+                {2, 3, 4, 5, 6 },
+                {2, 3, 4, 5, 6 },
+                {2, 3, 4, 5, 6 }
+            };
+            for (int y = 0; y < b.GetLength(0); y++)
+            {
+                for (int x = 0; x < b.GetLength(1); x++)
+                {
+                    Console.WriteLine(b[y, x] + "\t");
 
-            int[] a = { 1, 2, 3, 4, 5, 1,};
-
-            int b = a.Min();
-
-            int c = a.Max();
-
-            int d = a.Sum();
-
-            int e = a.Where(i => i % 2 == 0).Sum();
-
-            int f = a.Where(i => i % 2 != 0).Min();
-
-            int[] g = a.Distinct().ToArray();
-
-            int[] h = a.OrderBy(i => i).ToArray();
-
-            Console.WriteLine(c);
-
-            Console.WriteLine(b);
-
-            Console.WriteLine(d);
-
-            Console.WriteLine(e);
-
-            Console.WriteLine();
-
+                }
+                Console.WriteLine();
+            }
+            
             Console.ReadLine();
+
+
 
         }
     }
