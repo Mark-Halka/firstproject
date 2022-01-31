28c82a72119ba175015ee8cc283906503d55e82b
Mark-Halka <markhalka2929@gmail.com>
2022-01-31 19:34:10 +0200

    TWO-DIMENSIONAL ARROW part 2

--- lessons/lessons/Program.cs
+++ lessons/lessons/Program.cs
@@ -7,17 +7,24 @@ namespace lessons
     {
         static void Main(string[] args)
         {
-            int[,] b =
+            int[,] b = new int[2, 3];
+            
+            for (int i = 0; i < b.GetLength(0); i++)
             {
-                {2, 3, 4, 5, 6 },
-                {2, 3, 4, 5, 6 },
-                {2, 3, 4, 5, 6 }
-            };
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    Console.WriteLine("Y : " + i + ", X : " + j);
+                    b[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+
             for (int y = 0; y < b.GetLength(0); y++)
             {
                 for (int x = 0; x < b.GetLength(1); x++)
                 {
-                    Console.WriteLine(b[y, x] + "\t");
+                    Console.Write(b[y, x] + "\t");
 
                 }
                 Console.WriteLine();
